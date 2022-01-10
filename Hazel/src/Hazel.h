40bdb90e15b27cc8295c14375ba04031e17eb108
aikou <1159248631@qq.com>
2022-01-11 01:01:32 +0800

    Setup basic Application and Entry Point.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include<stdio.h>
+//For use by Hazel application
+
+#include "Hazel/application.h"
+
+//----Entry  Point---------
+#include "Hazel/EntryPoint.h"
+//--------------------------
